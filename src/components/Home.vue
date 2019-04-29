4cf55f5c3024a3eb1c0c9b1dcabf970ff0b94c1f
251814590 <251814590@163.com>
2019-04-29 10:24:58 +0800

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-container class="home-container">
+      <el-header class="home-header">
+        <span class="home_title">微人事</span>
+      </el-header>
+      <el-container>
+        <el-container>
+          <el-main>
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item v-text="this.$router.currentRoute.name"></el-breadcrumb-item>
+            </el-breadcrumb>
+            <keep-alive>
+              <router-view v-if="this.$route.meta.keepAlive"></router-view>
+            </keep-alive>
+            <router-view v-if="!this.$route.meta.keepAlive"></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
